d67d8639089f48141465b2af8023edbe8c18c076
zakhar <zaharsavchen@gmail.com>
2025-01-19 22:14:51 +0200

    Solution

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,32 @@
 from rest_framework import serializers
 
+from car.models import Car
+
 
 class CarSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField(read_only=True)
+    manufacturer = serializers.CharField(max_length=64, required=True)
+    model = serializers.CharField(max_length=64, required=True)
+    horse_powers = serializers.IntegerField(
+        min_value=1, max_value=1914, required=True
+    )
+    is_broken = serializers.BooleanField(required=True)
+    problem_description = serializers.CharField(required=False, allow_null=True)
+
+    def create(self, validated_data: dict) -> Car:
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance: Car, validated_data: dict) -> Car:
+        instance.manufacturer = validated_data.get(
+            "manufacturer", instance.manufacturer
+        )
+        instance.model = validated_data.get("model", instance.model)
+        instance.horse_powers = validated_data.get(
+            "horse_powers", instance.horse_powers
+        )
+        instance.is_broken = validated_data.get("is_broken", instance.is_broken)
+        instance.problem_description = validated_data.get(
+            "problem_description", instance.problem_description
+        )
+        instance.save()
+        return instance
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+import io
+
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    content = JSONRenderer().render(serializer.data)
+    return content
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    content = io.BytesIO(json)
+    data = JSONParser().parse(content)
+    serializer = CarSerializer(data=data)
+    if serializer.is_valid():
+        return Car.objects.create(**serializer.data)
+    return serializer.errors
